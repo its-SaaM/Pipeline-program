7af61a93468c11adec2ca30f4c43e2cab2308cd6
its-SaaM <srksumitkr831017@gmail.com>
2025-11-04 14:54:38 +0530

    added line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,5 @@ public class HelloWorld {
      public static void main(string args[])
      {
         System.out.println("Hello, This is pipeline project")
-     }
+        System.out.println("Added new line")
 }
