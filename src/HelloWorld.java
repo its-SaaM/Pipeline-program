1a77a0c00a0aed2b55395bd17d6c3bab4c3b4d5a
its-SaaM <srksumitkr831017@gmail.com>
2025-11-12 21:53:09 +0530

    Added maven project
    Added new project

--- src/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-
-public class HelloWorld {
-     public static void main(string args[])
-     {
-        System.out.println("Hello, This is pipeline project")
-        System.out.println("Added new line")
-}
